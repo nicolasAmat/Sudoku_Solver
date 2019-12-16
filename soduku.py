f268a8183e4dbeb79bbd267d7018b4e17092a3b7
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2019-12-16 20:44:54 +0100

    Solve Using SMTLIB

--- soduku.py
+++ soduku.py
@@ -1,60 +1,94 @@
 #!/usr/bin/env python3
 
+"""
+Project SAT/SMT Solving
+
+MoSIG HECS
+
+Student:  Nicolas AMat
+Profesor: David Monniaux
+"""
+
 import sys
+import os
+import subprocess
 from math import sqrt
 from z3 import Solver, Bool, Not, Or 
 
-class SodukuSolver:
-
+class SudokuSolver:
+	"""
+	Sudoku Solver using Z3
+	"""
+	
 	def __init__(self, filename):
-		self.grid = []
-		self.n = 0
-		self.s = 0
-		self.solver = Solver()
+		self.smt_filename = "{}.smt".format(filename)
+		self.smt = open(self.smt_filename, 'w')
+		
+		self.grid = [[0 for _ in range(9)] for _ in range(9)]
+		self.n = 9
+		self.s = 3
+
 		self.parseFile(filename)
-		self.checkGrid()
 
 	def parseFile(self, filename):
+		"""
+		Parse Sudoku grid
+		"""
 		try:
 			with open(filename, 'r') as file:
 				for l in file.readlines():
-					self.grid.append(l.replace('\n', '').split('|'))
+					value_indexed = l.replace('(', '').replace(')', '').replace('\n', '').split(',')
+					self.grid[int(value_indexed[0]) - 1][int(value_indexed[1]) - 1] = int(value_indexed[2])
 			file.close()
 
 		except FileNotFoundError as e:
 			exit(e)
 
-	def checkGrid(self):
-		self.n = len(self.grid)
-		for line in self.grid:
-			if len(line) != self.n:
-				exit("Grid not valid!")
-		self.s = int(sqrt(self.n))
-
 	def solveGrid(self):
+		"""
+		Solve Sudoku grid
+		- Write SMTlib file
+		- Solve using Z3
+		- Fill Sudoku grid
+		"""
+
+		# Create names of literals
 		literals = [\
 			[\
-			[Bool("{}:{}:{}".format(i, j, value)) for value in range(self.n)]\
+			["_{}_{}_{}_".format(i, j, value) for value in range(self.n)]\
 			for j in range(self.n)]\
 			for i in range(self.n)]
 
-		for i in range(self.n):
-			for j in range(self.n):
-				self.solver.add(Or(literals[i][j]))
+		# Declare literals
+		for line in literals:
+			for case in line:
+				for value in case:
+					self.smt.write("(declare-const {} Bool)\n".format(value))
+		
+		# Add constaints on cases
+		for line in literals:
+			for case in line:
+				values = ""
+				for value in case:
+					values += value + " "
+				self.smt.write("(assert (or {}))\n".format(values))
 
 		for value in range(self.n):
+			# Add constraints on lines
 			for i in range(self.n):
 				for j in range(self.n):
 					for j_ in range(self.n):
 						if j != j_:
-							self.solver.add(Or(Not(literals[i][j][value]), Not(literals[i][j_][value])))
+							self.smt.write("(assert (or (not {}) (not {})))\n".format(literals[i][j][value], literals[i][j_][value]))
 
+			# Add constraints on columns
 			for j in range(self.n):
 				for i in range(self.n):
 					for i_ in range(self.n):
 						if i != i_:
-							self.solver.add(Or(Not(literals[i][j][value]), Not(literals[i_][j][value])))
+							self.smt.write("(assert (or (not {}) (not {})))\n".format(literals[i][j][value], literals[i_][j][value]))
 
+			# Add constraints on squares
 			for square_i in range(self.s):
 				for square_j in range(self.s):
 					for i in range(self.s):
@@ -62,26 +96,41 @@ class SodukuSolver:
 							for j in range(self.s):
 								for j_ in range(self.s):
 									if i != i_ and j != j_:
-										self.solver.add(Or(Not(literals[square_i * self.s + i][square_j * self.s + j][value]), Not(literals[square_i * self.s + i_][square_j * self.s + j_][value])))
+										self.smt.write("(assert (or (not {}) (not {})))\n".format(literals[square_i * self.s + i][square_j * self.s + j][value], literals[square_i * self.s + i_][square_j * self.s + j_][value]))
 
+		# Add constraints on predifined values
 		for i in range(self.n):
 			for j in range(self.n):
-				if self.grid[i][j] != '.':
-					self.solver.add(literals[i][j][int(self.grid[i][j]) - 1])
+				if self.grid[i][j] != 0:
+					self.smt.write("(assert {})\n".format(literals[i][j][self.grid[i][j] - 1]))
 
-		if (not self.solver.check()):
-			exit(0)
+		# Check satisfiability
+		self.smt.write("(check-sat)")
+		# Get model
+		self.smt.write("(get-model)")
 
-		m = self.solver.model()
+		self.smt.close()
+		
+		# Run Z3
+		smt_result = subprocess.run(['z3', self.smt_filename], capture_output=True, text=True).stdout.split('\n')
+		
+		os.remove(self.smt_filename)
 
-		for i in range(self.n):
-			for j in range(self.n):
-				if self.grid[i][j] == ".":
-					for value, literal in enumerate(literals[i][j]):
-						if m.evaluate(literal):
-							self.grid[i][j] = value + 1
-							
+		# Exit if grid unsolvable
+		if (smt_result[0] != 'sat'):
+			exit("Grid unsolvable!")
+
+		# Parse the model and fill the grid
+		for line in range(2, len(smt_result), 2):
+			evaluation = smt_result[line + 1].replace(' ', '').replace(')', '')
+			if (evaluation == "true"):
+				literal = smt_result[line].split('_')
+				self.grid[int(literal[1])][int(literal[2])] = int(literal[3]) + 1 
+
 	def printGrid(self):
+		"""
+		Print Sudoku grid
+		"""
 		for line in self.grid:
 			print("|", end = '')
 			for element in line:
@@ -91,11 +140,12 @@ class SodukuSolver:
 
 
 if __name__=='__main__':
+
 	if (len(sys.argv) == 1):
 		exit("File missing: ./sodoku <path_to_file>")
-	solver = SodukuSolver(sys.argv[1])
+	
+	solver = SudokuSolver(sys.argv[1])
+	
 	solver.printGrid()
 	solver.solveGrid()
 	solver.printGrid()
-
-
